c2f59329e5fe22b7780dc88c58d5d205fdc165e9
LorranFranzDev <lorranfranzbastos1@gmail.com>
2022-07-27 20:26:48 -0300

    Resolvendo erros de código

--- app/src/main/java/me/dio/simulador/DetailActivity.kt
+++ app/src/main/java/me/dio/simulador/DetailActivity.kt
@@ -2,19 +2,20 @@ package me.dio.simulador
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import me.dio.simulador.databinding.ActivityMainBinding
+import me.dio.simulador.databinding.ActivityDetailBinding
+
 
 class DetailActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityDetailBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
         
         setSupportActionBar(binding.toolbar)
-        supportActionBar.setDefaultDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
